79a875db0933829dc7bbbe006fb2d4ca86ac1e0d
SihoonKim <ch10152080@gmail.com>
2024-07-10 13:23:49 +0900

    프로젝트 백 생성 완료

---
+++ back/hyune/src/main/java/com/ojt/hyune/HyuneApplication.java
@@ -0,0 +1,17 @@
+package com.ojt.hyune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@SpringBootApplication
+public class HyuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyuneApplication.class, args);
+		log.info("aass");
+	}
+
+}
---
+++ back/hyune/src/test/java/com/ojt/hyune/HyuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt.hyune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
